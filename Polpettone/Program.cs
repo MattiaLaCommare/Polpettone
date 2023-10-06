fedac6c4a04a33efc127272f17be554de852fad3
mattia.lacommare <mattia.lacommare@AULA136PC18>
2023-10-06 09:19:22 +0200

    Aggiungere i file di progetto.

---
+++ Polpettone/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Polpettone
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.WriteLine("Scrivi un numero da 1 a 7");
+            a = Convert.ToInt32(Console.ReadLine());
+            switch (a)
+            {
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("giorno lavorativo");
+                    break;
+                case 6:
+                    Console.WriteLine("sabato");
+                    break;
+                case 7:
+                    Console.WriteLine("giorno feriale");
+                    break;
+                default:
+                    Console.WriteLine("non è un giorno");
+                    break;
+            }   
+        }
+    }
+}
